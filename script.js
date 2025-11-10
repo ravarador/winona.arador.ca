96a10a82515476a18514fb2de967b94bd16c5cfb
Ravni Arador <ravarador@gmail.com>
2025-11-10 00:14:59 -0500

    Add project files.

---
+++ script.js
@@ -0,0 +1,7 @@
+// Smooth scroll function
+function scrollToSection(sectionId) {
+    const element = document.getElementById(sectionId);
+    if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+}
